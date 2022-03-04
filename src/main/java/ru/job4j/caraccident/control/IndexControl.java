ea13a9f0a78e190a0bcaca583e7c030f6e3fe589
olliestyle <olliestyle1989@gmail.com>
2022-03-04 16:03:00 +0300

    1. Загрузка данных в вид [#295782]

--- src/main/java/ru/job4j/caraccident/control/IndexControl.java
+++ src/main/java/ru/job4j/caraccident/control/IndexControl.java
@@ -4,10 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class IndexControl {
     @GetMapping("/")
     public String index(Model model) {
+        List<String> list = List.of("Murat", "Egor", "Pavel", "Oleg", "Anna", "Olga");
+        model.addAttribute("users", list);
         return "index";
     }
 }
