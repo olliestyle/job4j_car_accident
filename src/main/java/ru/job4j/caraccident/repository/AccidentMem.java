432a391bcdfaff946fc24909920ee37700b32383
olliestyle <olliestyle1989@gmail.com>
2022-03-06 12:08:33 +0300

    3. @ModelAttribute. Создание инцидента. [#261013 #272039].1

--- src/main/java/ru/job4j/caraccident/repository/AccidentMem.java
+++ src/main/java/ru/job4j/caraccident/repository/AccidentMem.java
@@ -6,11 +6,13 @@ import ru.job4j.caraccident.model.Accident;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Repository
 public class AccidentMem {
 
     private final Map<Integer, Accident> accidents = new HashMap<>();
+    private final AtomicInteger idCounter = new AtomicInteger(4);
 
     public AccidentMem() {
         accidents.put(1, new Accident(1, "one", "oneText", "oneAddress"));
@@ -23,8 +25,8 @@ public class AccidentMem {
     }
 
     public void create(Accident accident) {
-        accident.setId(accidents.keySet().size() + 1);
-        accidents.put(accidents.keySet().size() + 1, accident);
+        accident.setId(idCounter.get());
+        accidents.put(idCounter.getAndIncrement(), accident);
     }
 
     public void update(Accident accident) {
